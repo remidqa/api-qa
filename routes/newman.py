1c16b9ab8eeefe15eb6d7ce8e3ee8ca87681421f
Rémi <remi.dandois@gmail.com>
2023-09-05 18:40:32 -0400

    reworked behaviour with newman-runner API

--- routes/newman.py
+++ routes/newman.py
@@ -4,6 +4,10 @@ import json
 import urllib.request, urllib.parse, os
 from dotenv import load_dotenv 
 import requests
+from functions.send_webhooks import send_webhook
+import functions.mongodb as mongodb
+import functions.utils as utils
+
 
 load_dotenv()
 
@@ -15,8 +19,29 @@ api = Namespace('newman', description='Newman related operations')
 class runNewman(Resource):
     def post(self):
         coll_id = request.json["coll_id"]
-        url = f"{nemwman_runner_int_url}/run/coll_id/{coll_id}"
-        if ("env_id" in request.json.keys()):
-            url+= f"?env_id={request.json['env_id']}"
-        r = requests.post(url)
-        return f"Status Code: {r.status_code}, Content: {r.json()}"
+        run = requests.post(f"{nemwman_runner_int_url}/run/coll_id/{coll_id}")
+        report_name = run.json()["report_name"]
+        requested_report = requests.get(f"{nemwman_runner_int_url}/report?report_name={report_name}")
+        report = requested_report.json()
+        inserted_report = mongodb.insert_document("newman", report)
+        inserted_id = str(inserted_report.inserted_id)
+        deleted_report = requests.delete(f"{nemwman_runner_int_url}/report?report_name={report_name}")
+        deleted_msg = deleted_report.json()["msg"]
+        report_status = utils.get_report_status("newman", report)
+        webhook = send_webhook("newman", inserted_id, report_status)
+        return utils.send_json({
+            "status": 200,
+            "data": {
+                "newman_run": run.json(),
+                "requested_report": requested_report.json(),
+                "inserted_id_in_mongodb": inserted_id,
+                "webhook": webhook
+            }
+        })
+    #def post(self):
+    #    coll_id = request.json["coll_id"]
+    #    url = f"{nemwman_runner_int_url}/run/coll_id/{coll_id}"
+    #    if ("env_id" in request.json.keys()):
+    #        url+= f"?env_id={request.json['env_id']}"
+    #    r = requests.post(url)
+    #    return f"Status Code: {r.status_code}, Content: {r.json()}"
