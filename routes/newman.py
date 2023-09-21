a08a53de2b3671b3fd1b51712af7bfa4a86c9bf2
Rémi <remi.dandois@gmail.com>
2023-09-21 18:44:57 -0400

    execute postman collection from git config file

--- routes/newman.py
+++ routes/newman.py
@@ -7,7 +7,9 @@ import requests
 from functions.send_webhooks import send_webhook
 import functions.mongodb as mongodb
 import functions.utils as utils
-
+import functions.github as git
+import functions.testinyio as testinyio
+import functions.newman_runner as newman
 
 load_dotenv()
 
@@ -18,31 +20,39 @@ api = Namespace('newman', description='Newman related operations')
 @api.route("/run")
 class runNewman(Resource):
     def post(self):
-        coll_id = request.json["coll_id"]
-        env_id = request.json["env_id"] if "env_id" in request.json else ""
-        run = requests.post(f"{nemwman_runner_int_url}/run/coll_id/{coll_id}?{'env_id='+env_id if env_id != '' else ''}")
-        report_name = run.json()["report_name"]
-        requested_report = requests.get(f"{nemwman_runner_int_url}/report?report_name={report_name}")
-        report = requested_report.json()
-        inserted_report = mongodb.insert_document("newman", report)
+        # GET VARIABLES
+        app = request.json['app']
+        env = request.json['env']
+
+        # GET CONFIGURATION
+        github_conf = git.get_postman_conf(app, env)
+        coll_id = github_conf['postman_collection_id']
+        env_id = github_conf['postman_environment_id']
+        
+        # RUN COLLECTION
+        execution = newman.run_newman_and_get_report(coll_id, env_id)
+        report_status = utils.get_report_status("newman", execution['report'])
+        
+        # SAVE EXECUTION IN TESTS MANAGEMENT TOOL
+        tr = testinyio.report_test_execution( app, env, github_conf['testinyio_project_id'], github_conf['testinyio_testcase_id'], report_status, execution['report'] )
+        
+        # SAVE REPORT IN DATABASE
+        inserted_report = mongodb.insert_document("newman", execution['report'])
         inserted_id = str(inserted_report.inserted_id)
-        deleted_report = requests.delete(f"{nemwman_runner_int_url}/report?report_name={report_name}")
-        deleted_msg = deleted_report.json()["msg"]
-        report_status = utils.get_report_status("newman", report)
+        
+        # DELETE REPORT IN NEWMAN RUNNER
+        deleted_report = requests.delete(f"{nemwman_runner_int_url}/report?report_name={execution['report_name']}")
+
+        # SLACK NOTIFICATION
         webhook = send_webhook("newman", inserted_id, report_status)
+        
+        
         return utils.send_json({
             "status": 200,
             "data": {
-                "newman_run": run.json(),
-                "requested_report": requested_report.json(),
+                "tr": tr,
+                "newman_run": execution,
                 "inserted_id_in_mongodb": inserted_id,
                 "webhook": webhook
             }
         })
-    #def post(self):
-    #    coll_id = request.json["coll_id"]
-    #    url = f"{nemwman_runner_int_url}/run/coll_id/{coll_id}"
-    #    if ("env_id" in request.json.keys()):
-    #        url+= f"?env_id={request.json['env_id']}"
-    #    r = requests.post(url)
-    #    return f"Status Code: {r.status_code}, Content: {r.json()}"
